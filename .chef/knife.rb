5f7ae7456854db1441a4cec88f06cc14f6398b43
Matt Chun-Lum <mchunlum@gmail.com>
2014-03-17 20:30:12 -0700

    update knife.rb

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -4,6 +4,6 @@ node_name                user
 client_key               "#{ENV['HOME']}/.chef/#{user}.pem"
 validation_client_name   "chef-validator"
 validation_key           "#{ENV['HOME']}/.chef/chef-validator.pem"
-chef_server_url          "https://192.168.0.111"
+chef_server_url          "#{ENV['CHEF_SERVER']}"
 syntax_check_cache_path  "#{ENV['HOME']}/.chef/syntax_check_cache"
 cookbook_path            ["#{current_dir}/../cookbooks"]
